2de539bb959348b99b9222bbe6977f3c9448f832
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-02-14 15:56:47 +0800

    update require.js

---
+++ requirejs/leafdown/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lizixiang on 2017/2/14.
+ */
+require(["./leaf"], function (Leaf) {
+
+    var oContainer = document.getElementById('container');
+    var winWidth = document.documentElement.clientWidth;
+
+    //实例化出来四个叶子
+    for (var i = 0; i < 4; i++) {
+        var iWidth = 50+parseInt(Math.random()*51);
+        var leaf = new Leaf();
+    }
+
+});
