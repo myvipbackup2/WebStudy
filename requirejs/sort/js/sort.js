ecbd1c42479e0091dac1ce45a640966f11d46eb4
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-02-19 10:31:29 +0800

    update require

---
+++ requirejs/sort/js/sort.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lizixiang on 2017/2/19.
+ */
+
+define(['./isArr'], function (isArr) {
+
+    function sort(arr) {
+        if (!isArr(arr)) {
+            return;
+        }
+        arr.sort(function (a, b) {
+            return a - b;
+        });
+        return arr;
+    }
+
+    return sort;
+
+});
