320fee37552e6d56b44b004d35dae0f701973b38
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-02-19 15:30:55 +0800

    update require

--- requirejs/modal/js/main.js
+++ requirejs/modal/js/main.js
@@ -2,6 +2,16 @@
  * Created by lizixiang on 2017/2/19.
  */
 
-require(['jquery','modal'],function ($,Modal) {
+require(['jquery', 'modal'], function ($, Modal) {
+
+    $('#btn').on('click', function () {
+        Modal.open();
+    });
+
+    $('.modal-mask').on('click',function () {
+        Modal.close();
+    });
+
+
 
 });
--- requirejs/modal/js/modal.js
+++ requirejs/modal/js/modal.js
@@ -2,8 +2,36 @@
  * Created by lizixiang on 2017/2/19.
  */
 
-define(['jquery'],function ($) {
+define(['jquery'], function ($) {
 
-    
+    return {
+        open: function () {
+
+            var that = this;
+            this.$container = $('<div class="modal-container"></div>');
+            var $mask = $('<div class="modal-mask"></div>').on('click', function () {
+                that.close();
+            });
+            var $main = $('<div class="modal-main"></div>');
+            var $modalTitle = $('<div class="modal-title clearfix"></div>');
+            var $title = $('<span class="fl">这是标题</span>');
+            var $close = $('<span class="fr">[X]</span>').on('click', function () {
+                that.close();
+            });
+            var $content = $('<div class="modal-content"></div>');
+
+            $modalTitle.append($title).append($close);
+            $main.append($modalTitle).append($content);
+            this.$container.append($mask).append($main);
+            $(document.body).append(this.$container);
+
+        },
+
+        close: function () {
+            this.$container.remove();
+        }
+
+
+    }
 
 });
