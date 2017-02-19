23db1e6827f2516a00dcb61cd7e1bbaa80d28426
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-02-19 11:08:36 +0800

    update require

--- requirejs/sort/js/main.js
+++ requirejs/sort/js/main.js
@@ -2,11 +2,19 @@
  * Created by lizixiang on 2017/2/19.
  */
 
-require(['./sort'], function (sort) {
+// require(['./sort'], function (sort) {
+//
+//     // var arr = [1,34,4,2,6,8,42,44,61];
+//     var arr = '2322425';
+//     var val = sort(arr);
+//     console.log(val);
+//
+// });
 
-    // var arr = [1,34,4,2,6,8,42,44,61];
-    var arr = '2322425';
+define(function (require) {
+    var sort = require('./sort');
+    var arr = [1,34,4,2,6,8,42,44,61];
+    // var arr = '2322425';
     var val = sort(arr);
     console.log(val);
-
 });
--- requirejs/sort/js/sort.js
+++ requirejs/sort/js/sort.js
@@ -2,7 +2,25 @@
  * Created by lizixiang on 2017/2/19.
  */
 
-define(['./isArr'], function (isArr) {
+// define(['./isArr'], function (isArr) {
+//
+//     function sort(arr) {
+//         if (!isArr(arr)) {
+//             return;
+//         }
+//         arr.sort(function (a, b) {
+//             return a - b;
+//         });
+//         return arr;
+//     }
+//
+//     return sort;
+//
+// });
+
+define(function (require) {
+
+    var isArr = require('./isArr');
 
     function sort(arr) {
         if (!isArr(arr)) {
