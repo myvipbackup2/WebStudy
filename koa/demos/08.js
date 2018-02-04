5d13f8db7dea1c44b9ebe2cf62fc293fe62181cf
李子翔 <zixiangli@ctrip.com>
2018-02-04 19:31:24 +0800

    多个中间件会形成一个栈结构（middle stack）

--- koa/demos/08.js
+++ koa/demos/08.js
@@ -19,6 +19,17 @@ const three = (ctx, next) => {
   console.log('<< three');
 };
 
+/**
+ * 多个中间件会形成一个栈结构（middle stack），以"先进后出"（first-in-last-out）的顺序执行。
+ * 最外层的中间件首先执行。
+ * 调用next函数，把执行权交给下一个中间件。
+ * ...
+ * 最内层的中间件最后执行。
+ * 执行结束后，把执行权交回上一层的中间件。
+ * ...
+ * 最外层的中间件收回执行权之后，执行next函数后面的代码。
+ */
+
 app.use(one);
 app.use(two);
 app.use(three);
